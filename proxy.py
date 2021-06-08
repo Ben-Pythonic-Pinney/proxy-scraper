137a902529dc1b51934afedeb12edaa528a05637
Ben-Pythonic-Pinney <85582010+Ben-Pythonic-Pinney@users.noreply.github.com>
2021-06-08 21:21:58 +0100

    Add files via upload

---
+++ proxy.py
@@ -0,0 +1,15 @@
+# HTTP/s proxy-scaper
+# Created By: Lizard
+# Example: python[version] proxy.py
+
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.get('https://free-proxy-list.net')
+
+command = BeautifulSoup(r.content, 'html.parser')
+
+result = command.find('tbody')
+
+for i in result:
+    print(f'{i.find_all("td")[0].text}:{i.find_all("td")[1].text}')
